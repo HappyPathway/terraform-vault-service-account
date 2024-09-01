978957d6d5176d52fd6059da21c54923b47d563d
James Wurbel <jameswurbel@gmail.com>
2024-09-01 13:46:35 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,8 @@ locals {
 }
 
 resource "google_service_account" "vault_gcp_sa" {
-  count = var.create_service_account ? 1 : 0
+  count        = var.create_service_account ? 1 : 0
+  project      = var.project_id
   account_id   = var.service_account_id
   display_name = var.service_account_display_name
 }
@@ -28,7 +29,7 @@ resource "google_service_account_key" "vault_gcp_sa_key" {
 
 resource "google_project_iam_member" "vault_gcp_sa_iam" {
   // Assign IAM roles to the service account
-  for_each = toset(var.iam_roles)
+  for_each = var.create_service_account ? toset(var.iam_roles) : toset([]) 
   project  = var.project_id
   role     = each.value
   member   = "serviceAccount:${local.service_account.email}"
@@ -46,5 +47,5 @@ resource "vault_gcp_auth_backend_role" "gcp_role" {
   role                   = var.vault_gcp_auth_role_name
   type                   = "iam"
   bound_service_accounts = [local.service_account.email]
-  bound_projects         = ["test"]
+  bound_projects         = []
 }
