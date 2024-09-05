506dbbb490c61fb4ffb0e6c7c5f8403103e270bf
James Wurbel <jameswurbel@gmail.com>
2024-09-05 13:47:45 -0700

    Update main.tf (#2)
    * Update main.tf
    
    * Update main.tf
    
    * Update main.tf
    
    * Create terraform-test.yaml

--- main.tf
+++ main.tf
@@ -1,8 +1,15 @@
 locals {
   // Determine the service account to use based on the variable 'create_service_account'
-  service_account = var.create_service_account ? google_service_account.vault_gcp_sa : data.google_service_account.vault_gcp_sa
+  service_account = var.create_service_account ? one(google_service_account.vault_gcp_sa) : data.google_service_account.vault_gcp_sa
   // Extract the service account ID
   service_account_id = local.service_account.id
+}
+
+resource "google_service_account" "vault_gcp_sa" {
+  count        = var.create_service_account ? 1 : 0
+  project      = var.project_id
+  account_id   = var.service_account_id
+  display_name = var.service_account_display_name
 }
 
 # Note: This requires the Terraform to be run regularly
@@ -35,7 +42,7 @@ resource "google_service_account_key" "vault_gcp_sa_key" {
 
 resource "google_project_iam_member" "vault_gcp_sa_iam" {
   // Assign IAM roles to the service account
-  for_each = toset(var.iam_roles)
+  for_each = var.create_service_account ? toset(var.iam_roles) : toset([]) 
   project  = var.project_id
   role     = each.value
   member   = "serviceAccount:${local.service_account.email}"
@@ -53,5 +60,5 @@ resource "vault_gcp_auth_backend_role" "gcp_role" {
   role                   = var.vault_gcp_auth_role_name
   type                   = "iam"
   bound_service_accounts = [local.service_account.email]
-  bound_projects         = ["test"]
+  bound_projects         = []
 }
